cf8f95798e6600fed08f70cf856bcee63c85556b
samuel santhosh <samuel@arrivusystems.com>
2013-09-16 22:30:09 +0530

    tested tool tip

---
+++ tool_tip_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToolTipDemo::Application.config.secret_token = '1006249dc535aa12eca848d6c5bed1161b6f0a78703631fce11ae0e1ac09b9152962072e00aafbf7275400ba019aee669fd99e48cceba63d31c1ca5abf02092c'
